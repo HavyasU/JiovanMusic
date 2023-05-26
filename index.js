03843980ce49bc3a09794be2c3c7008f215733a6
HavyasRai <113080389+HavyasRai@users.noreply.github.com>
2023-05-26 20:53:44 +0530

    Update index.js
    ADDED COMMENT

--- index.js
+++ index.js
@@ -5,35 +5,35 @@ const path = require('path')
 const app = express();
 const exphbs = require('express-handlebars');
 var hbs = exphbs.create({ /* config */ });
-/* Running  APP.PY */
-try{
+// /* Running  APP.PY */
+// try{
 
 
-async function runapi() {
-  const { spawn } = require('child_process');
-  // Define the command to run the Python program
-  const pythonProcess = spawn('python', ['./savanapi/test.py']);
+// async function runapi() {
+//   const { spawn } = require('child_process');
+//   // Define the command to run the Python program
+//   const pythonProcess = spawn('python', ['./savanapi/test.py']);
 
-  // Listen for data from the Python program's stdout
-  pythonProcess.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
+//   // Listen for data from the Python program's stdout
+//   pythonProcess.stdout.on('data', (data) => {
+//     console.log(`stdout: ${data}`);
+//   });
 
-  // Listen for data from the Python program's stderr
-  pythonProcess.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-  });
+//   // Listen for data from the Python program's stderr
+//   pythonProcess.stderr.on('data', (data) => {
+//     console.log(`stderr: ${data}`);
+//   });
 
-  // Listen for the Python program to exit
-  pythonProcess.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
-}
-runapi()
-}
-catch(err){
-  console.log(err)
-}
+//   // Listen for the Python program to exit
+//   pythonProcess.on('close', (code) => {
+//     console.log(`child process exited with code ${code}`);
+//   });
+// }
+// runapi()
+// }
+// catch(err){
+//   console.log(err)
+// }
 /******************************************************************** */
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
