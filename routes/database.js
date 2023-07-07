298657699c45b0d719c2e64924d4f422f3b6edb8
Havyas Rai <havyasu2005@gmail.com>
2023-07-07 09:14:41 +0530

    database stopped

--- routes/database.js
+++ routes/database.js
@@ -1,51 +1,51 @@
 
 const express = require('express')
 const dsroute = express.Router();
-const mongoose = require('mongoose');
-const connectdb = async()=>{
-  await mongoose.connect("" ,{
-    dbName: 'jiovan',
-    retryWrites: true,
-    w: 'majority'
-  })
-}
-// connectdb().then(()=> console.log('i think conected')).catch((err)=>{
-//   console.log(err)
-// });
+// const mongoose = require('mongoose');
+// const connectdb = async()=>{
+//   await mongoose.connect("" ,{
+//     dbName: 'jiovan',
+//     retryWrites: true,
+//     w: 'majority'
+//   })
+// }
+// // connectdb().then(()=> console.log('i think conected')).catch((err)=>{
+// //   console.log(err)
+// // });
 
-let reviewschema = mongoose.Schema({
-  name:String,
-  rate:Number,
-  Review:String
-})
-let reviews = mongoose.model('reviews',reviewschema)
+// let reviewschema = mongoose.Schema({
+//   name:String,
+//   rate:Number,
+//   Review:String
+// })
+// let reviews = mongoose.model('reviews',reviewschema)
 
-let userreview ;
-dsroute.post('/RateUs',(req,res)=>{
-  userreview =  new reviews( 
-    {
-      name:req.body.name,
-      rate:req.body.rating,
-      Review:req.body.userreview
-    })
-    setTimeout(() => {
-      userreview.save().catch((err)=>{console.log(err)});
-    }, 1000);
+// let userreview ;
+// dsroute.post('/RateUs',(req,res)=>{
+//   userreview =  new reviews( 
+//     {
+//       name:req.body.name,
+//       rate:req.body.rating,
+//       Review:req.body.userreview
+//     })
+//     setTimeout(() => {
+//       userreview.save().catch((err)=>{console.log(err)});
+//     }, 1000);
     
-  })
-  let data;
-  const deleteall = async() =>{
-      data = await reviews.deleteMany({})
-      console.log(data)
-}
-dsroute.get('/reviewdata-havyas',(req,res)=>{
-  let rdata;
-  const finddata = async() =>{
-    rdata = await reviews.find()
-    console.log(rdata)
-    res.send(rdata)
-}
-finddata();
-})
-// deleteall();
+//   })
+//   let data;
+//   const deleteall = async() =>{
+//       data = await reviews.deleteMany({})
+//       console.log(data)
+// }
+// dsroute.get('/reviewdata-havyas',(req,res)=>{
+//   let rdata;
+//   const finddata = async() =>{
+//     rdata = await reviews.find()
+//     console.log(rdata)
+//     res.send(rdata)
+// }
+// finddata();
+// })
+// // deleteall();
 module.exports = dsroute;
